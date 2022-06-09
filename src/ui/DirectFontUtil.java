4c40ef9f0b87eacaec429f269e7c54e2566ba724
Shinovon <arman200711@gmail.com>
2022-06-10 00:32:15 +0500

    Fix NoSuchMethodError

--- src/ui/DirectFontUtil.java
+++ src/ui/DirectFontUtil.java
@@ -2,32 +2,31 @@ package ui;
 
 import javax.microedition.lcdui.Font;
 
+import cc.nnproject.utils.PlatformUtils;
+
 public class DirectFontUtil {
 	
 	private static boolean supported;
-
-	static {
-		try {
-			DirectUtilsInvoker.init();
-			supported = true;
-		} catch (NoClassDefFoundError e) {
-			supported = false;
-		} catch (Exception e) {
-			supported = false;
-		} catch (Error e) {
-			supported = false;
+	
+	public static void init() {
+		if(PlatformUtils.isSymbian3Based() || PlatformUtils.isAshaFullTouch()) {
+			try {
+				DirectUtilsInvoker.init();
+				supported = true;
+			} catch (Exception e) {
+			} catch (Error e) {
+			}
 		}
 	}
 	
 	public static Font getFont(int face, int style, int height, int size) {
-		//костыль
-		if(supported)
+		if(supported) {
 			try {
 				Font f = DirectUtilsInvoker.getFont(face, style, height);
-				if(f != null)
-					return f;
+				if(f != null) return f;
 			} catch (Throwable e) {
 			}
+		}
 		return Font.getFont(face, style, size);
 	}
 	
