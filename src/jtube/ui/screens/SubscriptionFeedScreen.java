0155a44e0f8112740222e9c0d71269581afd4220
Shinovon <arman200711@gmail.com>
2023-07-13 12:47:32 +0500

    Subscriptions feed

--- src/jtube/ui/screens/SubscriptionFeedScreen.java
+++ src/jtube/ui/screens/SubscriptionFeedScreen.java
@@ -1,13 +1,20 @@
 package jtube.ui.screens;
 
+import java.util.Vector;
+
+import cc.nnproject.json.JSONArray;
+import cc.nnproject.json.JSONObject;
+import jtube.App;
+import jtube.Constants;
 import jtube.Loader;
 import jtube.LocalStorage;
 import jtube.Settings;
-import jtube.models.ChannelModel;
 import jtube.models.VideoModel;
 import jtube.ui.items.VideoItem;
 
-public class SubscriptionFeedScreen extends NavigationScreen implements Runnable {
+public class SubscriptionFeedScreen extends NavigationScreen implements Runnable, Constants {
+
+	private int lastCount;
 
 	public SubscriptionFeedScreen() {
 		super("субскрибтионс");
@@ -15,7 +22,10 @@ public class SubscriptionFeedScreen extends NavigationScreen implements Runnable
 	
 	protected void show() { 
 		super.show();
-		new Thread(this).start();
+		if(lastCount != LocalStorage.getSubsciptions().length) {
+			new Thread(this).start();
+			lastCount = LocalStorage.getSubsciptions().length;
+		}
 		if(wasHidden) {
 			wasHidden = false;
 			if(Settings.videoPreviews) {
@@ -48,10 +58,36 @@ public class SubscriptionFeedScreen extends NavigationScreen implements Runnable
 	}
 
 	public void run() {
-		clear();
 		String[] subscriptions = LocalStorage.getSubsciptions();
-		for(int i = 0; i < subscriptions.length; i += 2) {
-			add(new ChannelModel(subscriptions[i], subscriptions[i + 1], null).makeListItem());
+		try {
+			Vector allVideos = new Vector();
+			for(int i = 0; i < subscriptions.length; i += 2) {
+				String id = subscriptions[i];
+				JSONObject r = (JSONObject) App.invApi("channels/" + id + "/latest?", "videos(" + VIDEO_FIELDS + ",published)");
+				JSONArray j = r.getArray("videos");
+				for(int k = 0; k < j.size(); k++) {
+					if(k > 10) break;
+					allVideos.addElement(j.get(k));
+				}
+			}
+			// sorting
+			for (int i = 0; i < allVideos.size() - 1; i++) {
+				for (int j = 0; j < i; j++) {
+					if (((JSONObject) allVideos.elementAt(j)).getLong("published") < ((JSONObject) allVideos.elementAt(j + 1)).getLong("published")) {
+						Object t = allVideos.elementAt(j);
+						allVideos.setElementAt(allVideos.elementAt(j + 1), j);
+						allVideos.setElementAt(t, j + 1);
+					}
+				}
+			}
+			for(int i = 0; i < allVideos.size(); i++) {
+				if(i > 100) break;
+				VideoModel v = new VideoModel((JSONObject) allVideos.elementAt(i));
+				if(Settings.videoPreviews && !Settings.lazyLoad) Loader.add(v);
+				add(v.makeListItem());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
